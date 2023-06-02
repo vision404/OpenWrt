3c49ec523d1b71d755edd310462ccff6547184e6
vision <134409474+vision404@users.noreply.github.com>
2023-06-02 14:24:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,14 +12,14 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#修改内存为32MB
-sed -i 's/<0x50000 0x7b0000>/<0x50000 0x1fb0000>/g' target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
-sed -i 's/7872k/32448k/g' target/linux/ramips/image/mt7620.mk
-#修改主题为argon
-sed -i 's/bootstrap/design/g' feeds/luci/collections/luci/Makefile
+#修改主题
 sed -i 's/Bootstrap/design/g' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/design/g' feeds/luci/collections/luci/Makefile
 #修改默认wifi名
 sed -i 's/OpenWrt/Vision/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/Openwrt/Vision/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #修改默认打开wifi，0打开，1关闭
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#修改flash为32MB
+sed -i 's/<0x50000 0x7b0000>/<0x50000 0x1fb0000>/g' target/linux/ramips/dts/mt7620a_phicomm_psg1218a.dts
+sed -i 's/7872k/32448k/g' target/linux/ramips/image/mt7620.mk
